c2d43d811f52721ed1b8b40a685a1e8bfb017654
Hemant Singh Parmar <111212867+hemant-code625@users.noreply.github.com>
2022-09-09 08:53:28 +0530

    Update swap.java

--- code/Functions & Methods  in Java/Sum Method/src/src/swap.java
+++ code/Functions & Methods  in Java/Sum Method/src/src/swap.java
@@ -1,13 +1,43 @@
 public class swap {
     public static void main(String[] args) {
-        //swapping the numbers
-
+      
         int a = 10;
         int b = 40;
         // Swapping numbers
-        int temp = a;
+        /* int temp = a;
         a = b;
         b = temp;
         System.out.println( a +" "+ b );
+
+        */
+
+        //Swapping of numbers using Function
+        swap(a ,b);
+        System.out.println(a +" "+b);
+
+        //How function works in string
+        String name="Hemant Singh Parmar";
+
+        ChangeName(name);
+
+        System.out.println(name);
+//        System.out.println(naam);  // Will not work and show error because naam will be printed only in that function where it is defined.
+
     }
+
+    static void ChangeName(String naam){
+        naam = "Rahul Rana";    //Here new object is created for same copy of reference variable called naam. Therefore, when we print name; object referred to naam will not be printed.
+
+        System.out.println(naam); //First code will go into function and print Rahul Rana and then will return to the ChangeName function.
+
+    }
+
+
+    static void swap (int a, int b){
+        int temp = a ;
+        a =b ;
+        b= temp ;
+    }
+
+
 }
