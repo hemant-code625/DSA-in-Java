e91e51433f5175a149bf1f6add7df6140080c24a
Hemant Singh Parmar <111212867+hemant-code625@users.noreply.github.com>
2022-11-20 20:50:13 +0530

    Update MathClass.java

--- code/About Java Language/src/MathClass.java
+++ code/About Java Language/src/MathClass.java
@@ -3,9 +3,17 @@ public class MathClass {
 
         //Math Class
         /*
-        1. Math.max()
-        2. Math.min()
-        3. Math.random() :- it returns a random value from range (0.1, 1.0)
+        1. Math.max(x,y)      only two numbers can be passed
+        2. Math.min(x,y)
+        3. Math.random()     return a random value from range (0.1, 1.0)
+        4. Math.sqrt(num)
+        5. Math.pow(x,y)     x^(y)
+        6. Math.log(x)       return the logarithm of x
+        7. Math.log10(x)     return the log when base is 10
+        8. Math.log1p(x)     return the log (x+1)
+        9. Math.exp(x)       return e^x
+        10.Math.expm1(x)     return e^x -1
+        
 
          */
         System.out.println(Math.max(123,444));
