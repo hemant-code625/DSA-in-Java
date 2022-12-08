8c1cc5d004f3fcfcea7eb7696f50a19c705bf799
Hemant Singh Parmar <111212867+hemant-code625@users.noreply.github.com>
2022-12-08 16:54:48 +0530

    Update SelectionSort.java

--- code/Recursion-Pattern Bubble and SelectionSort/src/SelectionSort.java
+++ code/Recursion-Pattern Bubble and SelectionSort/src/SelectionSort.java
@@ -1,29 +1,34 @@
-import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
         int []arr= {5,4,8,2,-3,10};
-        selectionSort(arr, arr.length-1,0);
+//        int[]arr={5,4,3,2,1};
+        selection(arr, arr.length,0,0);              //arr.length instead of arr.length-1
         System.out.println(Arrays.toString(arr));
     }
 
-    static void selectionSort(int []arr, int r, int c){
+    static void selection(int []arr, int r, int c, int max){
         if(r==0){                                                 //BASE CONDITION for recursion
             return;
         }
-        if(c<r){
+        if(c < r){
             //Find the largest number in that array
-
-
-            //swap arr[i] with arr[c] (i.e las index value of array)
-
-            selectionSort(arr, r, c+1);
-
+            if(arr[c]> arr[max]){
+                selection(arr, r, c+1, c);
+            }else{
+                selection(arr, r, c+1, max);    //max value is passed forward so to find the max value from the enitre row
+            }
         }
         else{
-            selectionSort(arr, r-1, 0);
+            //swap arr[i] with arr[c] (i.e las index value of array)
+            int temp = arr[r-1];        // swap with (row-1)th element not cth element
+            arr[r-1] = arr[max];
+            arr[max] = temp;
+
+            //new row is called
+            selection(arr, r-1, 0,0);
         }
 
-        }
+    }
 
 }
