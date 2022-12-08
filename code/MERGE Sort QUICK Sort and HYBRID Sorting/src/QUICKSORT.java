74774d1dcdd0f91d507bcb70c43aed50f68cd484
Hemant Singh Parmar <111212867+hemant-code625@users.noreply.github.com>
2022-12-08 17:00:46 +0530

    Add files via upload

---
+++ code/MERGE Sort QUICK Sort and HYBRID Sorting/src/QUICKSORT.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+
+public class QUICKSORT {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    static void quickSort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int s = low;
+        int e = high;
+        int mid = s + (e - s) / 2;                                    //pivot
+        while (s <= e) {
+
+            while (arr[s] < arr[mid]) {
+                s++;
+            }
+            while (arr[e] > arr[mid]) {
+                e--;
+            }
+            //this condition ensure that if array is already sorted then no need to swap
+            if (s <= e) {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+            }
+        }
+        //now pivot is at correct position please sort two halves now
+        quickSort(arr, low, e);
+        quickSort(arr, s, high);
+
+    }
+}
