23dbca9d0087519944b5d04aac30042b3e01635b
Hemant Singh Parmar <111212867+hemant-code625@users.noreply.github.com>
2023-03-10 14:58:22 +0530

    Update Main.java

--- OOP/src/com/hemant/AdvancedDataStructures/Main.java
+++ OOP/src/com/hemant/AdvancedDataStructures/Main.java
@@ -7,14 +7,16 @@ public class Main {
         list.insertFirst(10);
         list.insertFirst(9);
         list.insertFirst(5);
-        list.insertLast(100);
-        list.insert(101, 3);
+       list.insertLast(110);
+        
+        list.insert(101, 4);
 //        list.delete(3);
 //        list.delete(2);
         list.display();
         System.out.println(" ");
         System.out.println(list.deleteFirst());
-        System.out.println(list.deleteFirst());
+        System.out.println(list.deleteLast());
+        System.out.println(list.delete(2));
         list.display();
 
 
